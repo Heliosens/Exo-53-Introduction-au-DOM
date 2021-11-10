53f3a3b13a150360dc4fc4c6692fea63aa7ac716
Sylvie-B <unsolo@hotmail.fr>
2021-11-10 20:51:21 +0100

    manipulation du DOM

---
+++ app.js
@@ -0,0 +1,52 @@
+let para1 = document.getElementById('para1');
+let para2 = document.getElementById('para2');
+let para3 = document.getElementById('para3');
+let para4 = document.getElementById('para4');
+let para5 = document.getElementById('para5');
+
+para1.style.color = "purple";
+para2.style.color = "white";
+para3.style.color = "purple";
+para4.style.color = "white";
+para5.style.color = "purple";
+
+document.getElementById('spanTest1a').style.backgroundColor = "white";
+document.getElementById('spanTest1b').style.backgroundColor = "white";
+document.getElementById('spanTest2a').style.backgroundColor = "blue";
+document.getElementById('spanTest2b').style.backgroundColor = "blue";
+document.getElementById('spanTest3a').style.backgroundColor = "white";
+document.getElementById('spanTest3b').style.backgroundColor = "white";
+document.getElementById('spanTest4a').style.backgroundColor = "blue";
+document.getElementById('spanTest4b').style.backgroundColor = "blue";
+document.getElementById('spanTest5a').style.backgroundColor = "white";
+document.getElementById('spanTest5b').style.backgroundColor = "white";
+
+para1.style.width = "50%";
+para2.style.width = "50%";
+para3.style.width = "50%";
+para4.style.width = "50%";
+para5.style.width = "50%";
+
+para1.style.animationName = "textColor";
+para1.style.animationDuration = "4s";
+
+para4.style.animationName = "textColor";
+para4.style.animationDuration = "4s";
+
+document.getElementById('lien1').innerHTML = 'lien 1';
+document.getElementById('lien2').innerHTML = 'lien 2';
+document.getElementById('lien3').innerHTML = 'lien 3';
+document.getElementById('lien4').innerHTML = 'lien 4';
+document.getElementById('lien5').innerHTML = 'lien 5';
+
+document.getElementById('spanTest1a').style.direction = 'rtl';
+document.getElementById('spanTest1b').style.direction = 'rtl';
+document.getElementById('spanTest2a').style.direction = 'rtl';
+document.getElementById('spanTest2b').style.direction = 'rtl';
+document.getElementById('spanTest3a').style.direction = 'rtl';
+document.getElementById('spanTest3b').style.direction = 'rtl';
+document.getElementById('spanTest4a').style.direction = 'rtl';
+document.getElementById('spanTest4b').style.direction = 'rtl';
+document.getElementById('spanTest5a').style.direction = 'rtl';
+document.getElementById('spanTest5b').style.direction = 'rtl';
+
